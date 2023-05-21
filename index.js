2adf852db8015460d1a647394b347918721ecdae
emondewan00 <dewanmohammademon@gmail.com>
2023-05-21 15:12:07 +0600

    route wrap using mongodb function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
@@ -10,28 +11,57 @@ app.get("/", (req, res) => {
   res.send("Hallo baby toy server");
 });
 
-app.get("/allToys", async (req, res) => {
-  res.send();
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.jmqwkqq.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-app.get("/allToys/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  res.send();
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-app.patch("/allToys/:id", async (req, res) => {
-  const id = req.params.id;
-  const updatedToy = req.body;
-  const query = { _id: new ObjectId(id) };
-  res.send();
-});
+    app.get("/allToys", async (req, res) => {
+      res.send();
+    });
 
-app.delete("/myToys/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  res.send();
-});
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      res.send();
+    });
+
+    app.patch("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedToy = req.body;
+      const query = { _id: new ObjectId(id) };
+      res.send();
+    });
+
+    app.delete("/myToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      res.send();
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`listening on prot ${port}`);
